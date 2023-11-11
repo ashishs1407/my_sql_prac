618650812fcd8455e39a993f03978118599e9214
ashishs1407 <shimpi.ashish.me2@gmail.com>
2023-11-11 21:51:59 +0530

    added a intro code

---
+++ intro_to_sql.sql
@@ -0,0 +1,20 @@
+show databases ;
+
+select * from sakila.actor;
+
+select * from sakila.address;
+
+-- selecting a data base 
+use sakila ;
+
+select * from address1;
+
+select * from actor ;
+-- select <col names> from <table_name> ;
+select first_name , last_name from actor ;
+
+-- create database 
+-- create database <db_name> ; 
+# create a db my_prac  
+
+create database my_prac1 ;
